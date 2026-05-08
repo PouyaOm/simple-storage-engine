d29a3ee6482d5f4a61fdbd44ebb4383813ca55c9
WhoIsPo0 <pouyaomidi8413@gmail.com>
2026-05-08 19:58:22 +0330

    fixed bugs in record layout

--- storage_engine/record_layout.py
+++ storage_engine/record_layout.py
@@ -3,9 +3,9 @@ import struct
 def serialize_record(id, name, grade, is_active):
 
     
-    id_in_bytes = id.to_bytes(4, byteorder='little', signed='True')
+    id_in_bytes = id.to_bytes(4,'little')
     
-    name_in_bytes = name.encode("utf-8")
+    name_in_bytes = name.encode("utf-8")[:20]
     name_in_bytes = name_in_bytes.ljust(20, b' ')
 
     grade_in_bytes = struct.pack('<f', grade)
@@ -14,21 +14,15 @@ def serialize_record(id, name, grade, is_active):
     
     null_bitmap = (0).to_bytes(1, "little")
 
-    record = id_in_bytes + name_in_bytes + grade_in_bytes + is_active_in_bytes + null_bitmap
-    
-    assert len(record) == 30, f"Record size is not {30} bytes"
+    record = id_in_bytes + name_in_bytes + grade_in_bytes + is_active_in_bytes + null_bitmap   
     
     return record
 
 
 def deserialize_record(data):
-    
-    if len(data) != 30:
-        raise ValueError(f"Record must be {30} bytes")
-    
-    id = int.from_bytes(data[0:4], byteorder='little', signed=True)
+    id = int.from_bytes(data[0:4],'little')
     raw_name = data[4:24]
-    name = raw_name.decode('utf-8').strip()
+    name = data[4:24].decode('utf-8').strip()
     grade = struct.unpack('<f', data[24:28])[0]
     is_active = (data[28] == 1)
     
