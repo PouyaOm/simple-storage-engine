13a616e4ea4b895283a5b490d102aa80cb22226f
WhoIsPo0 <pouyaomidi8413@gmail.com>
2026-05-08 20:07:53 +0330

    fixed bugs in buffer manager

--- storage_engine/file_manager.py
+++ storage_engine/file_manager.py
@@ -1,27 +1,22 @@
 import os
 
-def  write_page(file, page_num, page_bytes):
-    os.makedirs(os.path.dirname(file), exist_ok=True)
-    if not os.path.exists(file):
-        with open(file, "wb") as f:
+def  write_page(file_path, page_num, page_bytes):
+    if not os.path.exists(file_path):
+        with open(file_path, "wb") as f:
             pass
     
-    with open(file, "rb+") as f:
+    with open(file_path, "rb+") as f:
         f.seek(page_num * 512)
         f.write(page_bytes)
-        f.flush()
 
         
-def read_page(file, page_num):
-    if not os.path.exists(file):
+def read_page(file_path, page_num):
+    if not os.path.exists(file_path):
         return None
     
-    with open(file, "rb") as f:
+    with open(file_path, "rb") as f:
         f.seek(page_num * 512)
         data = f.read(512)
-        
-    
-    if len(data) < 512:
-        return None
-    
-    return bytearray(data)
+        if len(data) < 512:
+            return None
+        return bytearray(data)
